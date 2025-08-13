1b91c9ac5c04f76caf7ef6d13fe36be6b45d83f4
pooris sharifi <pooriasharifi577@gmail.com>
2025-08-13 16:27:37 +0330

    ....

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -6,32 +6,57 @@ import { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate, useNavigation } from "react-router";
 import { CustomButton1 } from "~/component/componentButton";
 import ListComponent from "~/component/list";
-interface IMashinHesab {
-  Adad1: number
-  number1: number | null
-  number2: number | null
-  maghadir: string
-}
 
+export default function Home() {
+  const [result, setResult] = useState(0);
+  const [adad1, setAdad1] = useState<number | null>(null);
+  const [adad2, setAdad2] = useState<number | null>(null);
+  const [amal, setAmal] = useState<string | null>(null);
 
-export default function Home({ number1, number2 }: IMashinHesab) {
-  const [Adad1, setAdad] = useState(0);
-  const [currentId, setCurrentId] = useState<number | null>(null);
-  const [maghadir, setMaghadir] = useState(" ")
+  function handleClick(num: number) {
+    if (adad1 == null) {
+      setAdad1(num);
+    } else {
+      setAdad2(num);
+    }
+  }
 
   return (
     <div className="shadow-xl shadow-blue-800 w-150 h-150 ml-180 mt-50 ">
       <div>
-        <p>={Adad1}</p>
-        <p>{number1} {maghadir} {number2}</p>
+        <p>={result}</p>
+        <p>
+          {adad1} {amal} {adad2}
+        </p>
       </div>
       <div>
-        <CustomButton1 title={"X"} onClick={() => { setMaghadir("x") }} />
-        <CustomButton1 title="1" onClick={() => {}} />
-        <CustomButton1 title="2" onClick={() => { setCurrentId(2) }} />
+        <CustomButton1
+          title={"X"}
+          onClick={() => {
+            setAmal("*");
+          }}
+        />
+        <CustomButton1
+          title={"="}
+          onClick={() => {
+            setResult(adad1! * adad2!);
+            setAdad1(null);
+            setAdad2(null);
+          }}
+        />
+        <CustomButton1
+          title="1"
+          onClick={() => {
+            handleClick(1);
+          }}
+        />
+        <CustomButton1
+          title="2"
+          onClick={() => {
+            handleClick(2);
+          }}
+        />
       </div>
     </div>
-
-  )
-
+  );
 }
